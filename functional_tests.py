4f5c58e5805d9545cc48c7c7ed7c04ed14d9b8de
Igor_Chebotar <igorcebotar5@gmail.com>
2022-10-23 00:41:00 +0300

    functional_tests.py update

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -23,9 +26,33 @@ class NewVisitorTest(unittest.TestCase):
         # списках неотложных дел
 
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        #Она набирает в текстовом поле
+        "Купить павлиньи перья"
+        # (ее хобби – вязание рыболовных мушек)
+        inputbox.send_keys('Купить павлиньи перья')
+
+        # Когда она нажимает enter, страница обновляется, и теперь страница
+        # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+
+        self.fail('Закончить тест!')
 
 
 if __name__ == "__main__":
