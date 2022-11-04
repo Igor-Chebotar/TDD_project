f88c30b7471c08f6d533804fff8df1815d9081c6
Igor_Chebotar <igorcebotar5@gmail.com>
2022-11-04 19:52:51 +0300

    # покажет изменения в functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        '''подтверждение строки в таблице списка'''
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''тест: можно начать список и получить его позже'''
         # Эдит слышала про крутое новое онлайн-приложение со
@@ -36,7 +42,7 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
-        #Она набирает в текстовом поле
+        # Она набирает в текстовом поле
         "Купить павлиньи перья"
         # (ее хобби – вязание рыболовных мушек)
         inputbox.send_keys('Купить павлиньи перья')
@@ -46,9 +52,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент. Она
         # вводит "Сделать мушку из павлиньих перьев" (Эдит очень методична)
@@ -58,13 +62,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # Страница снова обновляется и теперь показывает оба элемента ее списка
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-        self.assertIn(
-            '2: Сделать мушку из павлиньих перьев',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
         self.fail('Закончить тест!')
 
