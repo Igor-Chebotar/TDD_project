cc830b75edb357adac35e2972c700d01dd0754e9
Igor_Chebotar <igorcebotar5@gmail.com>
2022-11-19 02:12:39 +0300

    URL-адреса для 2 форм, перемещен код в представления + тесты, новый URL-адрес

--- service/urls.py
+++ service/urls.py
@@ -20,5 +20,6 @@ from lists import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('lists/one', views.view_list, name='view_list')
+    path('lists/one', views.view_list, name='view_list'),
+    path('lists/new', views.new_list, name='new_list')
 ]
